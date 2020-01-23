f8271209024e454bc2b7e418b58585c2a6fd5acd
Jack Miras <jackmiras@gmail.com>
2020-01-23 00:09:55 -0300

    Trying to improve the code the shows battery level into the tmux status bar

--- bin/battery-indicator.sh
+++ bin/battery-indicator.sh
@@ -4,11 +4,11 @@
 HEART='♥ '
 
 if [[ $(uname) == 'Linux' ]]; then
-  current_charge=$(acpi | grep 'Discharging' | awk '{print $3}')
+  current_charge=$(acpi | grep 'Discharging' | awk '{print $4}')
   if [[ -z "${current_charge}" ]]; then
     current_charge=100
   else
-    current_charge=`expr "$current_chage" : '^\(.[1-9]*\)'`
+    current_charge=${current_charge::-2}
   fi
 
   total_charge=$(acpi | grep 'Full' | awk '{print $4}')
@@ -18,20 +18,26 @@ if [[ $(uname) == 'Linux' ]]; then
     total_charge=${total_charge::-1}
   fi
 else
-  battery_info=`ioreg -rc AppleSmartBattery`
-  current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-  total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+  battery_info=$(ioreg -rc AppleSmartBattery)
+  current_charge=$(echo "${battery_info}" | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+  total_charge=$(echo "${battery_info}" | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 fi
 
 charged_slots=$(echo "((($current_charge/$total_charge)*10)/3)+1" | bc -l | cut -d '.' -f 1)
+
 if [[ $charged_slots -gt 3 ]]; then
   charged_slots=3
 fi
 
 echo -n '#[fg=colour16]'
-for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
+
+for i in $(seq 1 $charged_slots); do
+  echo -n "$HEART"
+done
 
 if [[ $charged_slots -lt 3 ]]; then
   echo -n '#[fg=colour253]'
-  for i in `seq 1 $(echo "3-$charged_slots" | bc)`; do echo -n "$HEART"; done
+  for i in $(seq 1 "$(echo "3-$charged_slots" | bc)"); do
+    echo -n "$HEART"
+  done
 fi
