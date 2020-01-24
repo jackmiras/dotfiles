fca44cc734c78b7f89c38e30249d20ed40aad553
Jack Miras <jackmiras@gmail.com>
2020-01-24 17:14:25 -0300

    Installing zsh-autosuggestion and zsh-syntax-highlighting

--- zshrc-config/install-config.sh
+++ zshrc-config/install-config.sh
@@ -12,5 +12,11 @@ else
   # Creating symbolic link of Zsh configs into the home of computer.
   ln -s "$(pwd)/zshrc" "${HOME}/.zshrc"
 
+  # Installing zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM}"/plugins/zsh-autosuggestions
+
+  # Installing zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}"/plugins/zsh-syntax-highlighting
+
   echo 'Installation finished with success!'
 fi
