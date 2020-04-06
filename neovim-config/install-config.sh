88609b221144641d2d1ea3f11f92f6b376fa47e8
Jack Miras <jackmiras@gmail.com>
2020-04-05 21:02:52 -0300

    Removing unused symbolik links

--- neovim-config/install-config.sh
+++ neovim-config/install-config.sh
@@ -4,8 +4,6 @@
 
 # Creating symbolic links to Neovim.
 mkdir -p "$HOME/.config/nvim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/colors" "$HOME/.config/nvim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/indent" "$HOME/.config/nvim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/plugged" "$HOME/.config/nvim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/autoload" "$HOME/.config/nvim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/init.vim" "$HOME/.config/nvim"
@@ -13,8 +11,6 @@ ln -s "${HOME}/Projects/dotfiles/neovim-config/UltiSnips" "$HOME/.config/nvim"
 
 # Creating symbolic links to Vim.
 mkdir -p "$HOME/.vim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/colors" "$HOME/.vim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/indent" "$HOME/.vim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/plugged" "$HOME/.vim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/autoload" "$HOME/.vim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/UltiSnips" "$HOME/.vim"
