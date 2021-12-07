a0e7480e32cf7c66449d1221fc68c9fb73ed01ea
Jack Miras <jackmiras@gmail.com>
2021-12-07 14:59:58 -0300

    Adding function to install LanguageTool CLI

--- install.sh
+++ install.sh
@@ -219,6 +219,20 @@ function install_azure_cli(){
   curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 }
 
+function install_languagetool() {
+  echo "#######################################################################"
+  echo "# Installing LanguageTool                                             #"
+  echo "#######################################################################"
+
+  wget https://languagetool.org/download/LanguageTool-stable.zip
+
+  unzip LanguageTool-stable.zip && rm -rf LanguageTool-stable.zip
+
+  mv LanguageTool* languagetool && mv languagetool "${HOME}/.languagetool"
+
+  rm -rf LanguageTool*
+}
+
 function git_global_configs() {
   echo "#######################################################################"
   echo "# Configure Git globals                                               #"
@@ -342,11 +356,13 @@ function main() {
   install_pgcli
   install_litecli
   install_docker
+  install_terraform
   install_neovim
   install_aws_cli
   install_kubectl
   install_linters
   install_azure_cli
+  install_languagetool
   git_global_configs
 
   install_snaps
