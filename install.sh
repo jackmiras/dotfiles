6f2181e1ef6a9f839521ac3fb2dd8415258b7efe
Jackson Miras <jackmiras@gmail.com>
2022-10-21 11:31:45 -0300

    added function to install starship

--- install.sh
+++ install.sh
@@ -21,6 +21,14 @@ function install_zsh() {
   sudo apt-get install -y zsh
 }
 
+function install_starship() {
+  echo "#######################################################################"
+  echo "# Installing Starship                                                 #"
+  echo "#######################################################################"
+
+  curl -sS https://starship.rs/install.sh | sh
+}
+
 function install_ccat() {
   echo "#######################################################################"
   echo "# Installing ccat                                                     #"
@@ -376,6 +384,7 @@ function main() {
 
   install_ag
   install_zsh
+  install_starship
   install_ccat
   install_entr
   install_tmux
