798f978d1fe9b3fbe53365533604ea810037ac35
Jackson Miras <jackmiras@gmail.com>
2022-10-21 11:34:02 -0300

    increased battery indicator to 5 and changed the battery icon from a ♥ to 

--- tmux-config/bin/battery-indicator.sh
+++ tmux-config/bin/battery-indicator.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-# modified from http://ficate.com/blog/2012/10/15/battery-life-in-the-land-of-tmux/
 
-HEART='♥ '
+# BATTERY_SYMBOL='♥ '
+BATTERY_SYMBOL=' '
+
+# Amount of heart slots we wanna show at Tmux status bar
+# Eg: 5 hearts means that every heart represents 20% batery usage
+BATTERY_SLOTS=5
+
+# This multiplier is proportional to the number of BATTERY_SLOTS
+# if the slots double this number should double as well
+BATTERY_MULTIPLIER=20 # This should
 
 if [[ $(uname) == 'Linux' ]]; then
   current_charge=$(acpi | grep 'Discharging' | awk '{print $4}')
@@ -23,21 +31,21 @@ else
   total_charge=$(echo "${battery_info}" | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 fi
 
-charged_slots=$(echo "((($current_charge/$total_charge)*10)/3)+1" | bc -l | cut -d '.' -f 1)
+charged_slots=$(echo "(((${current_charge}/${total_charge})*${BATTERY_MULTIPLIER})/${BATTERY_SLOTS})+1" | bc -l | cut -d '.' -f 1)
 
-if [[ $charged_slots -gt 3 ]]; then
-  charged_slots=3
+if [[ $charged_slots -gt $BATTERY_SLOTS ]]; then
+  charged_slots=$BATTERY_SLOTS
 fi
 
 echo -n '#[fg=colour16]'
 
 for i in $(seq 1 $charged_slots); do
-  echo -n "$HEART"
+  echo -n "$BATTERY_SYMBOL"
 done
 
-if [[ $charged_slots -lt 3 ]]; then
+if [[ $charged_slots -lt $BATTERY_SLOTS ]]; then
   echo -n '#[fg=colour253]'
-  for i in $(seq 1 "$(echo "3-$charged_slots" | bc)"); do
-    echo -n "$HEART"
+  for i in $(seq 1 "$(echo "${BATTERY_SLOTS}-${charged_slots}" | bc)"); do
+    echo -n "$BATTERY_SYMBOL"
   done
 fi
