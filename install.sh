ef27eed4efb32d5265caba6a70b049f3344b03aa
Jackson Miras <jackmiras@gmail.com>
2023-01-19 11:46:07 -0300

    updated terraform installation process

--- install.sh
+++ install.sh
@@ -140,9 +140,19 @@ function install_terraform() {
   echo "# Installing Terraform                                                #"
   echo "#######################################################################"
 
-  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-  sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  sudo apt install terraform
+  wget -O- https://apt.releases.hashicorp.com/gpg | \
+      gpg --dearmor | \
+      sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+  gpg --no-default-keyring \
+      --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+      --fingerprint
+
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+      https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+      sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+  sudo apt update && sudo apt-get install terraform
 }
 
 function install_neovim() {
