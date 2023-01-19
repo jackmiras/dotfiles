861c03f48c8b92bd575dc9e5175b4f74834cdea6
Jackson Miras <jackmiras@gmail.com>
2023-01-19 11:43:14 -0300

    removed unused code from neovim install script

--- neovim-config/install-config.sh
+++ neovim-config/install-config.sh
@@ -5,18 +5,11 @@
 # Creating plugged dir
 mkdir -p "${HOME}/Projects/dotfiles/neovim-config/plugged"
 
-# Creating symbolic links to Neovim.
+# Creating symbolic links
 mkdir -p "$HOME/.config/nvim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/plugged" "$HOME/.config/nvim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/autoload" "$HOME/.config/nvim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/init.vim" "$HOME/.config/nvim"
 ln -s "${HOME}/Projects/dotfiles/neovim-config/UltiSnips" "$HOME/.config/nvim"
-
-# Creating symbolic links to Vim.
-mkdir -p "$HOME/.vim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/plugged" "$HOME/.vim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/autoload" "$HOME/.vim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/UltiSnips" "$HOME/.vim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/init.vim" "$HOME/.vimrc"
 
 echo 'Installation finished with success!'
