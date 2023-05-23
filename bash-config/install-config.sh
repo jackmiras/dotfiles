acf87fdbcf081f161bf102d587dc60f1605ce095
Jackson Miras <jackmiras@gmail.com>
2023-05-23 14:46:09 -0400

    fixed bash symlink process

--- bash-config/install-config.sh
+++ bash-config/install-config.sh
@@ -3,7 +3,7 @@
 # This apply custom configurations on Bash.
 
 rm -rf "${HOME}/.bash_profile"
-ln -s "${HOME}/Projects/dotfiles/bash-config/bash_profile" "${HOME}/.bash_profile"
-ln -s "${HOME}/Projects/dotfiles/bash-config/starship.toml" "${HOME}/.config/starship.toml"
+ln -s "${HOME}/Projects/dotfiles/bash-config/bash_profile" "${HOME}/.bash_profile" || true # Continue if symlink exists
+ln -s "${HOME}/Projects/dotfiles/bash-config/starship.toml" "${HOME}/.config/starship.toml" || true # Continue if symlink exists
 
 echo 'Installation finished with success!'
