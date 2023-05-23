482d4caee4eec692c5e1f0740f8fcb48cdd3102c
Jackson Miras <jackmiras@gmail.com>
2023-05-23 14:45:41 -0400

    fixed neovim symlink process

--- neovim-config/install-config.sh
+++ neovim-config/install-config.sh
@@ -7,9 +7,9 @@ mkdir -p "${HOME}/Projects/dotfiles/neovim-config/plugged"
 
 # Creating symbolic links
 mkdir -p "$HOME/.config/nvim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/plugged" "$HOME/.config/nvim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/autoload" "$HOME/.config/nvim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/init.vim" "$HOME/.config/nvim"
-ln -s "${HOME}/Projects/dotfiles/neovim-config/UltiSnips" "$HOME/.config/nvim"
+ln -s "${HOME}/Projects/dotfiles/neovim-config/plugged" "$HOME/.config/nvim" || true # Continue if symlink exists
+ln -s "${HOME}/Projects/dotfiles/neovim-config/autoload" "$HOME/.config/nvim" || true # Continue if symlink exists
+ln -s "${HOME}/Projects/dotfiles/neovim-config/init.vim" "$HOME/.config/nvim" || true # Continue if symlink exists
+ln -s "${HOME}/Projects/dotfiles/neovim-config/UltiSnips" "$HOME/.config/nvim" || true # Continue if symlink exists
 
 echo 'Installation finished with success!'
