bc4bd1d18157d83cf6a8d0816a4122a532811a38
Jackson Miras <jackmiras@gmail.com>
2023-05-23 14:45:58 -0400

    fixed tmux symlink process

--- tmux-config/install-config.sh
+++ tmux-config/install-config.sh
@@ -6,8 +6,8 @@ if [[ ! -d "${HOME}/.tmux" ]]; then
   mkdir "${HOME}/.tmux"
 fi
 
-ln -s "${HOME}/Projects/dotfiles/tmux-config/themes" "${HOME}/.tmux/themes"
-ln -s "${HOME}/Projects/dotfiles/tmux-config/tmux.conf" "${HOME}/.tmux.conf"
-ln -s "${HOME}/Projects/dotfiles/tmux-config/plugins" "${HOME}/.tmux"
+ln -s "${HOME}/Projects/dotfiles/tmux-config/themes" "${HOME}/.tmux/themes" || true # Continue if symlink exists
+ln -s "${HOME}/Projects/dotfiles/tmux-config/tmux.conf" "${HOME}/.tmux.conf" || true # Continue if symlink exists
+ln -s "${HOME}/Projects/dotfiles/tmux-config/plugins" "${HOME}/.tmux" || true # Continue if symlink exists
 
 echo 'Installation finished with success!'
