8392937b3068c3d54c2ce132decaeb4361a23932
Jackson Miras <jackmiras@gmail.com>
2023-06-19 23:51:39 -0300

    fixed bash configuration installation

--- bash-config/install-config.sh
+++ bash-config/install-config.sh
@@ -2,8 +2,14 @@
 # Script written by: JM.
 # This apply custom configurations on Bash.
 
-rm -rf "${HOME}/.bash_profile"
-ln -s "${HOME}/Projects/dotfiles/bash-config/bash_profile" "${HOME}/.bash_profile" || true # Continue if symlink exists
+if [[ "$(uname -s)" == "Linux" ]]; then
+  rm -rf "${HOME}/.bashrc"
+  ln -s "${HOME}/Projects/dotfiles/bash-config/bash_profile" "${HOME}/.bashrc" || true # Continue if symlink exists
+elif [[ "$(uname -s)" == "Darwin" ]]; then
+  rm -rf "${HOME}/.bash_profile"
+  ln -s "${HOME}/Projects/dotfiles/bash-config/bash_profile" "${HOME}/.bash_profile" || true # Continue if symlink exists
+fi
+
 ln -s "${HOME}/Projects/dotfiles/bash-config/starship.toml" "${HOME}/.config/starship.toml" || true # Continue if symlink exists
 
 echo 'Installation finished with success!'
